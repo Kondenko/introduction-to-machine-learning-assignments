d7b54d52663ad3c5d60479a9c803333b5cc252d6
Vladimir <kondenko2011@gmail.com>
2018-04-02 12:15:43 +0300

    Update utils

---
+++ PA6-SVM/pa6.py
@@ -0,0 +1,15 @@
+from sklearn.svm import SVC
+from utils import *
+import pandas as pd
+
+e = Executor()
+
+dataset = pd.read_csv(get_csv_path("svm-data"), header=None)
+x = dataset.iloc[:, 1:]
+y = dataset.iloc[:, 0]
+
+clf = SVC(random_state=241, C=100000, kernel='linear')
+clf.fit(x, y)
+
+answer = ','.join(str(n + 1) for n in clf.support_)
+e.print_answer("Support object indices", answer)
--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def get_csv_path(name):
 
 def get_titanic_dataset(pandas):
     return pandas.read_csv(
-        filepath_or_buffer=(get_datasets_folder() + "titanic\\titanic.csv"),
+        filepath_or_buffer=(get_datasets_folder() + "titanic/titanic.csv"),
         index_col='PassengerId',
         engine='python'
     )
@@ -43,13 +43,13 @@ class Executor:
     def write_to_file(name, text):
         import errno
         try:
-            answers_dir = "answers"
+            answers_dir = "./answers"
             try:
                 os.makedirs(answers_dir)
             except OSError as e:
                 if e.errno != errno.EEXIST:
                     raise
-            answer_file = "{}\\{}.txt".format(answers_dir, str(name))
+            answer_file = "{}/{}.txt".format(answers_dir, str(name))
             with open(answer_file, "w") as f:
                 f.write(str(text))
         except IOError:
@@ -61,8 +61,5 @@ class Executor:
         if write_to_file:
             self.write_to_file(title, answer)
 
-    def execute(self, title, algorithm):
-        self.print_title(title)
-        answer = algorithm()
-        print answer
-        self.write_to_file(title, answer)
+    def execute(self, title, algorithm, write_to_file = True):
+        self.print_answer(title, algorithm(), write_to_file)
