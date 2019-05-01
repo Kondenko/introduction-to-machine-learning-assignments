5a6014b801fed807c920fd73e6eff03c5aa9b7f3
Vladimir Kondenko <kondenko2011@gmail.com>
2019-05-01 18:53:22 +0300

    Organize the code

--- PA10-SalaryPrediction/pa10.py
+++ PA10-SalaryPrediction/pa10.py
@@ -7,6 +7,8 @@ from sklearn.linear_model import Ridge
 
 from utils import *
 
+# Variables
+
 col_desc = "FullDescription"
 col_salary = "SalaryNormalized"
 
@@ -14,7 +16,7 @@ e = Executor()
 
 tfidf_vectorizer = TfidfVectorizer(min_df=5)
 
-dictVectorizer = DictVectorizer()
+dict_vectorizer = DictVectorizer()
 
 ridge = Ridge(alpha=1, random_state=241)
 
@@ -22,11 +24,11 @@ data_train: DataFrame = p.read_csv(get_csv_dataset("salary-train"))
 data_test: DataFrame = p.read_csv(get_csv_dataset("salary-test-mini"))
 
 
-data_train[col_desc] = data_train[col_desc].apply(lambda x: x.lower())
-data_train[col_desc] = data_train[col_desc].replace('[^a-zA-Z0-9]', ' ', regex=True)
+# Preprocessing
 
-desc_vectorized_train = tfidf_vectorizer.fit_transform(data_train[col_desc])
-desc_vectorized_test = tfidf_vectorizer.transform(data_test[col_desc])
+def simplify_text(data):
+    data[col_desc] = data[col_desc].apply(lambda x: x.lower())
+    data[col_desc] = data[col_desc].replace('[^a-zA-Z0-9]', ' ', regex=True)
 
 
 def dict_vectorize(transform_func, data):
@@ -37,24 +39,29 @@ def fix_missing_values(column):
     data_train[column].fillna('nan', inplace=True)
 
 
+simplify_text(data_train)
+simplify_text(data_test)
+
+desc_vectorized_train = tfidf_vectorizer.fit_transform(data_train[col_desc])
+desc_vectorized_test = tfidf_vectorizer.transform(data_test[col_desc])
+
 fix_missing_values("LocationNormalized")
 fix_missing_values("ContractTime")
 
-X_train_categ = dict_vectorize(dictVectorizer.fit_transform, data_train)
-X_test_categ = dict_vectorize(dictVectorizer.transform, data_test)
+X_train_categ = dict_vectorize(dict_vectorizer.fit_transform, data_train)
+X_test_categ = dict_vectorize(dict_vectorizer.transform, data_test)
 
 X_train = hstack([desc_vectorized_train, X_train_categ])
 y_train = data_train[col_salary]
 
 X_test = hstack([desc_vectorized_test, X_test_categ])
 
+# Prediction
 
 ridge.fit(X_train, y_train)
 
 predictions = ridge.predict(X_test)
 
-answer = ",".join(list(map(lambda n: round2(n), predictions)))
+answer = " ".join(list(map(lambda n: str(round2(n)), predictions)))
 
-title = "Predicted salary"
-
-e.print_answer(title, answer)
+e.print_answer("Predicted salary", answer)
