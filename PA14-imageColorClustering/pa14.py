e3acc54cf828a492f28b342402bcdd7674a45119
Vladimir Kondenko <kondenko2011@gmail.com>
2019-12-29 17:21:16 +0300

    Use a custom PSNR implementation

--- PA14-imageColorClustering/pa14.py
+++ PA14-imageColorClustering/pa14.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 import skimage
 from matplotlib import pylab
 from skimage.io import imread
-from skimage.metrics import peak_signal_noise_ratio
 from sklearn.cluster import KMeans
 import numpy as np
 from numpy import ndarray
@@ -15,13 +14,13 @@ imgarr: np.ndarray = skimage.img_as_float(image)
 
 X: ndarray = imgarr.reshape((-1, imgarr.shape[2]))
 
+
 def clusterize(n_clusters=8, show_images=False) -> float:
     clf = KMeans(init="k-means++", random_state=241, n_clusters=n_clusters)
     clf.fit(X)
     # Mean
     X_mean = list(map(lambda p: clf.cluster_centers_[p], clf.labels_))
-    mse_mean = mse(np.array(X_mean))
-    psnr_mean = peak_signal_noise_ratio(X, np.array(X_mean))
+    psnr_mean = psnr(X, np.array(X_mean))
     if show_images:
         image_mean = np.reshape(X_mean, image.shape)
         fig_mean = pylab.figure()
@@ -36,8 +35,7 @@ def clusterize(n_clusters=8, show_images=False) -> float:
     for c in clusterized_data.keys():
         cluster_medians[c] = np.median(clusterized_data[c], 0)
     X_median = list(map(lambda p: cluster_medians[p], clf.labels_))
-    mse_median = mse(np.array(X_median))
-    psnr_median = peak_signal_noise_ratio(X, np.array(X_median))
+    psnr_median = psnr(X, np.array(X_median))
     if show_images:
         image_median = np.reshape(X_median, image.shape)
         fig_median = pylab.figure()
@@ -49,16 +47,14 @@ def clusterize(n_clusters=8, show_images=False) -> float:
     return max(psnr_mean, psnr_median)
 
 
-def psnr(mse, max=1):
-    return 20 * np.log(max / np.sqrt(mse))
+def psnr(X, X_hat):
+    max = 1
+    mse = get_mse(X, np.array(X_hat))
+    return 20 * np.log10(max) - 10 * np.log10(mse)
 
 
-def mse(X_hat, shape: tuple = image.shape):
-    return np.sum(sqr_abs(X, X_hat, 0) + sqr_abs(X, X_hat, 1) + sqr_abs(X, X_hat, 2)) / (3 * shape[0] * shape[1])
-
-
-def sqr_abs(X, X_hat, channel):
-    return np.power(np.abs(X[:, channel] - X_hat[:, channel]), 2)
+def get_mse(X, X_hat):
+    return np.sum(np.power(X - X_hat, 2)) / np.product(image.shape)
 
 
 def find_min_num_of_clusters_for_psnr(max_clusters: int = 20, target: int = 20):
