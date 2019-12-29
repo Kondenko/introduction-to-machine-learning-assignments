cec7dc2cfebdfd7be7e4f8a681aa98ee2dcd8bfb
Vladimir Kondenko <kondenko2011@gmail.com>
2019-12-29 16:20:57 +0300

    Add the algorithm for finding the minimum number of clusters for best PSNR

--- PA14-imageColorClustering/pa14.py
+++ PA14-imageColorClustering/pa14.py
@@ -1,30 +1,32 @@
 from collections import defaultdict
 
-import matplotlib
 import skimage
-import matplotlib as mpt
 from matplotlib import pylab
 from skimage.io import imread
 from sklearn.cluster import KMeans
 import numpy as np
 from numpy import ndarray
+from utils import Executor
 
 image = imread("./parrots.jpg")
 
 imgarr: np.ndarray = skimage.img_as_float(image)
 
-X: ndarray = imgarr.reshape((-1, imgarr.shape[2]))  # [:20]  # REMOVE [20]
+X: ndarray = imgarr.reshape((-1, imgarr.shape[2]))
 
 
-def clusterize() -> float:
-    clf = KMeans(init="k-means++", random_state=241)
+def clusterize(n_clusters=8, show_images=False) -> float:
+    clf = KMeans(init="k-means++", random_state=241, n_clusters=n_clusters)
     clf.fit(X)
     # Mean
     X_mean = list(map(lambda p: clf.cluster_centers_[p], clf.labels_))
-    image_mean = np.reshape(X_mean, image.shape)
-    fig_mean = pylab.figure()
-    fig_mean.canvas.set_window_title("Mean")
-    pylab.imshow(image_mean)
+    mse_mean = mse(np.array(X_mean))
+    psnr_mean = psnr(mse_mean)
+    if show_images:
+        image_mean = np.reshape(X_mean, image.shape)
+        fig_mean = pylab.figure()
+        fig_mean.canvas.set_window_title("Mean")
+        pylab.imshow(image_mean)
     # Median
     clusterized_data: dict = defaultdict(list)
     for i in range(0, len(clf.labels_) - 1):
@@ -34,16 +36,24 @@ def clusterize() -> float:
     for c in clusterized_data.keys():
         cluster_medians[c] = np.median(clusterized_data[c], 0)
     X_median = list(map(lambda p: cluster_medians[p], clf.labels_))
-    image_median = np.reshape(X_median, image.shape)
-    fig_median = pylab.figure()
-    fig_median.canvas.set_window_title("Median")
-    pylab.imshow(image_median)
-    # MSE for Mean
-    mse_mean = mse(X, np.array(X_mean), image.shape)
-    return 0
+    mse_median = mse(np.array(X_median))
+    psnr_median = psnr(mse_median)
+    if show_images:
+        image_median = np.reshape(X_median, image.shape)
+        fig_median = pylab.figure()
+        fig_median.canvas.set_window_title("Median")
+        pylab.imshow(image_median)
+    print(f"{n_clusters} clusters")
+    print(f"PSNR for mean color = {psnr_mean}")
+    print(f"PSNR for median color = {psnr_median}")
+    return min(psnr_mean, psnr_median)
 
 
-def mse(X, X_hat, shape: tuple):
+def psnr(mse, max=1):
+    return 20 * np.log(max / np.sqrt(mse))
+
+
+def mse(X_hat, shape: tuple = image.shape):
     return np.sum(sqr_abs(X, X_hat, 0) + sqr_abs(X, X_hat, 1) + sqr_abs(X, X_hat, 2)) / (3 * shape[0] * shape[1])
 
 
@@ -51,6 +61,20 @@ def sqr_abs(X, X_hat, channel):
     return np.power(np.abs(X[:, channel] - X_hat[:, channel]), 2)
 
 
-clusterize()
+def find_min_num_of_clusters_for_psnr(max_clusters: int = 20, target: int = 20):
+    for clusters in range(1, max_clusters):
+        psnr = clusterize(clusters)
+        if psnr > target:
+            print(f"Reached target PSNR {psnr} with {clusters} clusters")
+            return clusters
+    print("The algorithm did not converge")
+    return None
+
+
+e = Executor()
+
+min_num_of_clusters = find_min_num_of_clusters_for_psnr()
+
+e.print_answer("Minimal number of clusters", min_num_of_clusters)
 
 pylab.show()
